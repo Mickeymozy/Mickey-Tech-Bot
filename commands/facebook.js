9a128a705c34d35b052d1d6f3a7efcaa24c694b4
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-08-12 09:27:51 +0300

    Update facebook.js

--- commands/facebook.js
+++ commands/facebook.js
@@ -26,7 +26,7 @@ async function facebookCommand(sock, chatId, message) {
         });
 
         // Fetch video data from API
-        const response = await axios.get(`https://api.diioffc.web.id/api/download/facebook?url=${url}`);
+        const response = await axios.get(`https://apis-keith.vercel.app/download/fbdown?url=${url}`);
         const data = response.data;
 
         if (!data || data.status !== 200 || !data.facebook || !data.facebook.sdVideo) {
